b07d66842aaadfd1ca9784f072f05a1b6ac49d19
pablorodriguez <uo190909@gmail.com>
2019-02-01 19:22:42 +0100

    SDI - 3.2 Contexto aplicación y 3.3 listado dinámico”

---
+++ src/com/uniovi/sdi/Producto.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.uniovi.sdi;
+
+/**
+ * @author Pablo Rodríguez Valdés
+ *
+ * SDI - 2019
+ */
+public class Producto {
+	
+	private String nombre;
+	private String imagen;
+	private float precio;
+	
+	
+	
+	/**
+	 * @param nombre
+	 * @param imagen
+	 * @param precio
+	 */
+	public Producto(String nombre, String imagen, float precio) {
+		super();
+		this.nombre = nombre;
+		this.imagen = imagen;
+		this.precio = precio;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the imagen
+	 */
+	public String getImagen() {
+		return imagen;
+	}
+	/**
+	 * @param imagen the imagen to set
+	 */
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	/**
+	 * @return the precio
+	 */
+	public float getPrecio() {
+		return precio;
+	}
+	/**
+	 * @param precio the precio to set
+	 */
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	
+}
